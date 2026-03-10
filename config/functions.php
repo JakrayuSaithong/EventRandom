d729998a02c50b7f6e3e91931029757db0a139e7
Dew <158989279+JakrayuSaithong@users.noreply.github.com>
2026-03-10 10:13:17 +0700

    Update functions.php

--- config/functions.php
+++ config/functions.php
@@ -106,14 +106,4 @@ function dbLastInsertId($conn) {
     return $result ? (int)$result[0]['id'] : null;
 }
 
-/**
- * Decrypt function (matches index.php pattern)
- */
-function decryptIt($q)
-{
-	$cryptKey  = 'Iloveyouallpann';
-	$qDecoded = rtrim(openssl_decrypt(base64_decode($q), "AES-256-CBC", md5($cryptKey), 0, substr(md5(md5($cryptKey)), 0, 16)), "\0");
-	//write_file("temp.txt","=".date("H:i:s")."=qDecoded===$qDecoded=\r\n","a");
-	return ($qDecoded);
-}
 ?>
